1bd130ba5f4640b693f3f2d453bf3e1b9b53e6ee
lemo7242 <leon.moll@esolutions.de>
2019-12-30 14:01:58 +0100

    add stopwatch and countdown functions

---
+++ globalFuncs.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+# credit https://superuser.com/a/611582/362137
+function countdown(){
+   date1=$((`date +%s` + $1)); 
+   while [ "$date1" -ge `date +%s` ]; do 
+     echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
+     sleep 0.1
+   done
+}
+# credit https://superuser.com/a/611582/362137
+function stopwatch(){
+  date1=`date +%s`; 
+   while true; do 
+    echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r"; 
+    sleep 0.1
+   done
+}
